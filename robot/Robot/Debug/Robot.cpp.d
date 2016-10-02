b23ebb0d52f0fd09d7ed16841356c0e76f0648c5
USHERBROOKE\duss2503 <samuel.dussault@usherbrooke.ca>
2016-10-02 18:27:25 -0400

    Projet de départ, incluant le Sonar

---
+++ robot/Robot/Debug/Robot.cpp.d
@@ -0,0 +1,21 @@
+C:\Users\duss2503\AppData\Local\Temp\VMicroBuilds\Robot\uno\Robot.cpp.o: \
+ C:\Users\duss2503\AppData\Local\Temp\VMicroBuilds\Robot\uno\Robot.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\duss2503\Documents\Arduino\libraries\NewliquidCrystal/LiquidCrystal_I2C.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\duss2503\Documents\Arduino\libraries\NewliquidCrystal/I2CIO.h \
+ C:\Users\duss2503\Documents\Arduino\libraries\NewliquidCrystal/LCD.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\duss2503\AppData\Local\Temp\VMicroBuilds\Robot\uno\Sonar.h \
+ C:\Users\duss2503\AppData\Local\Temp\VMicroBuilds\Robot\uno\ISonar.h \
+ C:\Users\duss2503\Documents\Arduino\libraries\NewPing/NewPing.h
---
+++ robot/Robot/ISonar.h
@@ -0,0 +1,10 @@
+#ifndef ISONAR_H
+#define ISONAR_H
+
+class ISonar
+{
+public:
+	virtual unsigned int ping_cm() = 0;
+};
+
+#endif // !ISONAR_H
