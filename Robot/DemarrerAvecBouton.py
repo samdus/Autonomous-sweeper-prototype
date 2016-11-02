c2ee528d739c45b644deec99e9f5f1604d295440
USHERBROOKE\duss2503 <samuel.dussault@usherbrooke.ca>
2016-11-01 22:54:07 -0400

    Thread pour les boutons

--- Robot/DemarrerAvecBouton.py
+++ Robot/DemarrerAvecBouton.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from threading import Thread
 
-#adjust for where your switch is connected
-boutonJaunePin = 17
+def threaded_function():
+  #adjust for where your switch is connected
+  boutonJaunePin = 17
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(boutonJaunePin,GPIO.IN)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(boutonJaunePin,GPIO.IN)
 
-while True:
-  if (GPIO.input(boutonJaunePin)):
-    os.system("make -C ~/Documents/Projet-RepoGit/gsd/Robot run")
+  while True:
+    if (GPIO.input(boutonJaunePin)):
+      os.system("make -C ~/Documents/Projet-RepoGit/gsd/Robot run")
+
+thread = Thread(target = threaded_function)
+thread.start()
--- Robot/FermerAvecBouton.py
+++ Robot/FermerAvecBouton.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from threading import Thread
 
-#adjust for where your switch is connected
-boutonRougePin = 19
+def threaded_function():
+  #adjust for where your switch is connected
+  boutonRougePin = 19
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(boutonRougePin,GPIO.IN)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(boutonRougePin,GPIO.IN)
 
-while True:
-  if (GPIO.input(boutonRougePin)):
-    os.system("sudo pkill ExempleCom")
-    os.system("stty -F /dev/ttyACM0 -hupcl")
+  while True:
+    if (GPIO.input(boutonRougePin)):
+      os.system("sudo pkill ExempleCom")
+      os.system("stty -F /dev/ttyACM0 -hupcl")
+
+thread = Thread(target = threaded_function)
+thread.start()
