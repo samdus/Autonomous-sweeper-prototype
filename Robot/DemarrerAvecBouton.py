9675944bb4cd7c68f7685f65e211730aaec7f2e8
USHERBROOKE\duss2503 <samuel.dussault@usherbrooke.ca>
2016-11-01 23:07:55 -0400

    Deux boutons, sans thread

--- Robot/DemarrerAvecBouton.py
+++ Robot/DemarrerAvecBouton.py
@@ -1,18 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 import os
-from threading import Thread
 
-def threaded_function():
-  #adjust for where your switch is connected
-  boutonJaunePin = 17
+#adjust for where your switch is connected
+boutonJaunePin = 17
 
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(boutonJaunePin,GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(boutonJaunePin,GPIO.IN)
 
-  while True:
-    if (GPIO.input(boutonJaunePin)):
-      os.system("make -C ~/Documents/Projet-RepoGit/gsd/Robot run")
-
-thread = Thread(target = threaded_function)
-thread.start()
+while True:
+  if (GPIO.input(boutonJaunePin)):
+    os.system("make -C ~/Documents/Projet-RepoGit/gsd/Robot run")
