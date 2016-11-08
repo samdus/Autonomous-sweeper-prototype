d53d77d66ac42b2e265ec209b32060ebcf143dd4
DINF-0014-46A\dinf0014_admin <g_abriel10@hotmail.com>
2016-11-08 16:07:22 -0500

    Affichge de map de robot, data en dure dans le fichier

--- DomotiqueManager/imports/ui/mapDisplay/MapContainer.jsx
+++ DomotiqueManager/imports/ui/mapDisplay/MapContainer.jsx
@@ -3,44 +3,125 @@ import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 
-
+var robotMap = [{
+x1:150,
+y1:100,
+x2:200,
+y2:25,
+nbpts:5,
+},
+{
+x1:200,
+y1:25,
+x2:200,
+y2:150,
+nbpts:5,
+},
+{
+x1:200,
+y1:150,
+x2:150,
+y2:100,
+nbpts:5,
+},]
 class MapContainer extends Component {
   constructor(props) {
     super(props);
 
-  }
-  componentDidMount(){
+      this.draw = this.draw.bind(this)
+   }
 
-    var robotMapWidth = 200;
-    var robotMapHeight = 200;
-    var actualWidth = 0;
-    var actuelHeight = 0;
-    /*Get our wrapper and acual canvas*/
+
+    componentWillMount() {
+
+        
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.draw);
+    }
+
+  draw(){
     var mapCanvas=document.getElementById("mapCanvas");
     var wrapper =document.getElementById("mapCanvas").parentNode;
     var ctx = mapCanvas.getContext("2d");
+    //Erase the surface
+    ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
 
-    /*Set the dimensions of the canvas to match its parent*/
+
+    var minX = minY =9999999;
+    var maxX= maxY =(-Number.MAX_VALUE);
+    robotMap.map( function(item) {
+      if(item.x1 < minX)
+        minX = item.x1
+      if(item.x2 < minX)
+        minX = item.x2
+      if(item.y1 < minY)
+        minY = item.y2
+      if(item.y2 < minY)
+        minY = item.y2
+
+      if(item.x1 > maxX)
+        maxX = item.x1
+      if(item.x2 > maxX)
+        maxX = item.x2
+      if(item.y1 > maxY)
+        maxY = item.y2
+      if(item.y2 > maxY)
+        maxY = item.y2
+    })
+    var cadrantFixX = 0;
+    var cadrantFixY = 0;
+    if(minX<0){
+      cadrantFixX = Math.abs(minX);
+    }
+    if(minY<0){
+      cadrantFixY = Math.abs(minY);
+    }
+    //Map width and height from the robot
+    var robotMapWidth = maxX + cadrantFixX;
+    var robotMapHeight = maxY + cadrantFixY;
+    //Canvas width and height
+    var actuelHeight = 0;
+    var actualWidth = 0;
+
+    /*Get our wrapper and acual canvas*/
+    var maxCanvasWidth =  wrapper.offsetWidth;
+    var maxCanvasHeight =  wrapper.offsetHeight;
+    
+
     actualWidth = ctx.canvas.width  = wrapper.offsetWidth;
-    actuelHeight = ctx.canvas.height = wrapper.offsetHeight;
-
-
+    actuelHeight = ctx.canvas.height = (robotMapHeight / robotMapWidth) * actualWidth;
+    if(actuelHeight > maxCanvasHeight){
+      var resizeRatio = maxCanvasHeight  / actuelHeight;
+      actuelHeight = ctx.canvas.height = actuelHeight * resizeRatio;
+      actualWidth = ctx.canvas.width  = (robotMapWidth / robotMapHeight) * actuelHeight;
+    }
+    var scaleX = actualWidth / robotMapWidth;
+    var scaleY = actuelHeight / robotMapHeight;
     /*Draw a simple triangle*/
-    ctx.beginPath();
-    ctx.moveTo(this.getRealPointX(150, robotMapWidth, actualWidth) ,this.getRealPointY(100, robotMapHeight, actuelHeight));
-    ctx.lineTo(this.getRealPointX(200, robotMapWidth, actualWidth) ,this.getRealPointY(150, robotMapHeight, actuelHeight));
-    ctx.lineTo(this.getRealPointX(200, robotMapWidth, actualWidth) ,this.getRealPointY(25, robotMapHeight, actuelHeight));
-    ctx.fill();
+    ctx.strokeStyle = '#ff0000';
+    ctx.lineWidth = 2;
+    robotMap.map( function(item) {
+      ctx.beginPath();
+      ctx.moveTo((item.x1+cadrantFixX)*scaleX, (item.y1+cadrantFixY)*scaleY);
+      ctx.lineTo((item.x2+cadrantFixX)*scaleX, (item.y2+cadrantFixY)*scaleY);
+      ctx.stroke();
+    })
+    ctx.fillStyle="#FFFFFF";
+    //position robot?
+    ctx.fillRect(cadrantFixX,cadrantFixY,15,15);
+
   }
-  getRealPointX(pointRobot, robotMapWidth ,mapWidth){
-    return pointRobot*(mapWidth/robotMapWidth);
-  }
-  getRealPointY(pointRobot, robotMapHeight,mapHeight){
-    return pointRobot*(mapHeight/robotMapHeight);
+
+  componentDidMount(){
+    window.addEventListener("resize", this.draw);
+    this.draw();
   }
   render() {
         return (
+
             <div className="canvas-wrapper" id="canvawrapper">
+
               <canvas id="mapCanvas" width="300" height="150" />
             </div>
       );
@@ -55,3 +136,12 @@ export default createContainer(() => {
   return {
   };
 }, MapContainer);
+
+/*
+    var arrayX = robotMap.map(function(item){return item.x1}).concat(robotMap.map(function(item){return item.x2}));
+    var arrayY = robotMap.map(function(item){return item.y1}).concat(robotMap.map(function(item){return item.y2}));
+    var minXtest = Math.min.apply( null, arrayX );
+    var maxXtest = Math.max.apply( null, arrayX );
+    var minYtest = Math.min.apply( null, arrayY );
+    var maxYtest = Math.max.apply( null, arrayY );
+ */
