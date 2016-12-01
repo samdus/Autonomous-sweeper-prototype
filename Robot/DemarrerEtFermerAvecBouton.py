f564e8ad77347d8936e5b2fe3bae84c2d46e4e6a
USHERBROOKE\duss2503 <samuel.dussault@usherbrooke.ca>
2016-12-01 13:24:32 -0500

    Enlever ./

--- Robot/DemarrerEtFermerAvecBouton.py
+++ Robot/DemarrerEtFermerAvecBouton.py
@@ -8,7 +8,7 @@ def callback_rouge(channel):
     os.system("stty -F /dev/ttyACM0 -hupcl &")
 
 def callback_jaune(channel):
-    os.system("sudo ./home/pi/Desktop/Demarre-balayeuse")
+    os.system("sudo /home/pi/Desktop/Demarre-balayeuse")
     #os.system("make -C /home/pi/Documents/Projet-RepoGit/gsd/Robot run &")
 
 boutonRougePin = 19
